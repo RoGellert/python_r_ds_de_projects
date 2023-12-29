18629b3dfa76911cb3deb1ce0a4882139057b97b
Roman Gellert <titangtace@gmail.com>
2023-12-29 16:17:41 +0100

    Added fake_data.py

--- Airflow/fake_data.py
+++ Airflow/fake_data.py
@@ -1,6 +1,15 @@
 from faker import Faker
+import pandas
 
 if __name__ == "__main__":
     fake_generator = Faker()
+    num_of_fake_profiles = 10000
 
-    print(fake_generator.profile())
+    data = []
+    for i in range(num_of_fake_profiles):
+        if i % 1000 == 0:
+            print(i)
+        data.append(fake_generator.profile())
+
+    df = pandas.DataFrame(data)
+    df.to_csv("./fake_data", index=False, index_label=False)
