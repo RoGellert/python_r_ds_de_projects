17de5165506a8a620a793f2d4461f31c7901ae4a
Roman Gellert <titangtace@gmail.com>
2024-02-05 14:22:13 +0100

    Fake data creation and insertion

--- posgres_db_docker/fake_data_gen.py
+++ posgres_db_docker/fake_data_gen.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
 
     fake_data = []
 
-    for _ in range(0, number_of_entries):
-        fake_data.append([fake.name(), fake.address()])
-
-    print([i for i in fake_data])
+    for _ in range(number_of_entries):
+        name_surname = fake.name()
+        name_surname = name_surname.split()
+        name, surname = name_surname[0], name_surname[1]
+        print(f"('{name}', '{surname}', '{fake.email()}', '{fake.date()}'),")
