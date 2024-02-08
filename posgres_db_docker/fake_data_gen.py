c1ed6cf5087708f92e2322b5bc8979850ef0d1c4
Roman Gellert <titangtace@gmail.com>
2024-02-08 13:48:12 +0100

    Adding movies

--- posgres_db_docker/fake_data_gen.py
+++ posgres_db_docker/fake_data_gen.py
@@ -4,9 +4,33 @@ fake = Faker()
 
 
 def fake_movies(number_of_entries):
+    movie_add = ""
+
     for _ in range(number_of_entries):
-        rand_int = random.randrange(0, 100)
-        print(f"(''),")
+        movie_num = random.randrange(1, 5)
+        match movie_num:
+            case 1:
+                movie_add = "'s film"
+            case 2:
+                movie_add = "'s movie"
+            case 3:
+                movie_add = "'s debut"
+            case 4:
+                movie_add = "'s art house"
+        movie_name = fake.name() + movie_add
+
+        rating = ""
+        rating_num = random.randrange(1, 4)
+        match rating_num:
+            case 1:
+                rating = "PG 13"
+            case 2:
+                rating = "R"
+            case 3:
+                rating = "G"
+
+        print(f"('{movie_name}','{fake.date()}', {round(random.uniform(40, 70), 2)}, '{rating}', "
+              f"{round(random.uniform(1, 5), 1)}),")
 
 
 def fake_users(number_of_entries):
